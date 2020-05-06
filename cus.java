17ff3683c575ef1378d9a85fa04398de286a1a7c
Vinita99 <63218570+Vinita99@users.noreply.github.com>
2020-05-06 09:17:29 +0530

    Add files via upload

---
+++ cus.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hangman;
+
+/**
+ *
+ * @author KOTRESHA
+ */
+class cus {
+    
+    private String name;
+    private int number;
+    public cus(String name,int number)
+    {
+    this.name=name;
+    this.number=number;
+        
+    }
+    public String getname()
+    {return name;}
+    public int getnum()
+    {
+        return number;
+    }
+        
+    
+    
+}
